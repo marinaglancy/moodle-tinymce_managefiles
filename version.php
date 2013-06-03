fe0a80f8ca695ea1628bced89b750d1d79189c86
Marina Glancy <marina@moodle.com>
2013-06-03 16:26:54 +1000

    tinymce_managefiles Manage files embedded in text editor

--- version.php
+++ version.php
@@ -26,4 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2013060200;  // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2012120300;  // Required Moodle version.
+$plugin->release   = '1.0';
+$plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'tinymce_managefiles';  // Full name of the plugin (used for diagnostics).
