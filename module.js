c80f77d4362365eddae33cf1a9e817d2825fe21b
Marina Glancy <marina@moodle.com>
2013-06-04 16:37:39 +1000

    make js/css compartible with moodle 2.5

--- module.js
+++ module.js
@@ -1,13 +1,14 @@
 M.tinymce_managefiles = M.tinymce_managefiles || {}
 M.tinymce_managefiles.analysefiles = function(Y) {
     var form = Y.one('#tinymce_managefiles_manageform'),
-        usedfiles, missingfiles = '', i
+        usedfiles, missingfiles = '', i, delfilesfieldset
     if (!form || !window.parent || !window.parent.tinyMCE.activeEditor) {
         return;
     }
     usedfiles = window.parent.tinyMCE.activeEditor.execCommand('mceManageFilesUsedFiles')
+    var delfilesfieldset = form.one('#deletefiles,#id_deletefiles')
     for (i in usedfiles) {
-        if (!form.one('#deletefiles .felement.fcheckbox input[name="deletefile[' + usedfiles[i] + ']"]')) {
+        if (!delfilesfieldset.one('.felement.fcheckbox input[name="deletefile[' + usedfiles[i] + ']"]')) {
             missingfiles += '<li>' + usedfiles[i] + '</li>';
         }
     }
@@ -15,7 +16,7 @@ M.tinymce_managefiles.analysefiles = function(Y) {
         form.addClass('hasmissingfiles')
         form.one('.managefilesstatus').setContent(M.str.tinymce_managefiles.hasmissingfiles + ' <ul>' + missingfiles + '</ul>')
     }
-    form.all('#deletefiles .felement.fcheckbox').each(function(el) {
+    delfilesfieldset.all('.felement.fcheckbox').each(function(el) {
         var chb = el.one('input[type=checkbox]'),
             match = /^deletefile\[(.*)\]$/.exec(chb.get('name'));
         if (match && usedfiles.indexOf(match[1]) === -1) {
